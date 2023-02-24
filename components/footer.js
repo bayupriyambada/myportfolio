8a2211d724fc88a0e8580bd156d4c7d16596480c
bayupb <kampuspb.bayu@gmail.com>
2023-02-25 00:56:51 +0700

    add padding in footer 2

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="md:mt-10 mt-5 lg:p-0 p-5">
-      <span className="text-xl text-white">Simply designed by Bpriyambadam 🙌</span>
+      <span className="text-xl text-white">Simply designed by Bpriyambadam 🙌 - 2023</span>
     </div>
   )
 }
